7c29b0bda51853f98e126730edb564f0728c5fd6
panorange <man.freemailer@gmail.com>
2018-02-26 17:22:13 +0200

    new path for img for GHpages

---
+++ src/components/AnimatedMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="animated-menu">
+    <div class="menu-toggler"></div>
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Features</a></li>
+      <li><a href="#">Pricing</a></li>
+      <li><a href="#">Blog</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="sass" scoped>
+  @import '../sass/variables'
+  @import '../sass/mixins'
+
+  .animated-menu
+
+</style>

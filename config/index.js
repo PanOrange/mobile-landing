d68d3381da81b5db89e4b847fc5c3518cf7c27a3
panorange <man.freemailer@gmail.com>
2018-02-26 17:33:20 +0200

    add assetsSubDirectory: mobile-landing/static

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/mobile-landing',
+    assetsSubDirectory: 'mobile-landing/static',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps

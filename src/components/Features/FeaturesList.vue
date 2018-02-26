b41856aae360acf44d19a33414b41f6c94a6eacd
panorange <man.freemailer@gmail.com>
2018-02-26 13:57:43 +0200

    add FeaturesList animation

--- src/components/Features/FeaturesList.vue
+++ src/components/Features/FeaturesList.vue
@@ -2,27 +2,37 @@
   <div class="features-list">
     <div class="container-fixed">
       <div class="block">
-        <div class="column-center">
-          <h1>Main Features</h1>
-          <p>Shocase main features of your app or tell them how the app works using a three step process. Its easy. You may also add some descriptions to this section.</p>
-        </div>
+        <transition
+          appear
+          v-on:enter="onEnter"
+        >
+          <div class="column-center">
+            <h1>Main Features</h1>
+            <p>Shocase main features of your app or tell them how the app works using a three step process. Its easy. You may also add some descriptions to this section.</p>
+          </div>
+        </transition>
       </div>
     </div>
     <div class="container">
-      <div class="gallery">
-        <ul>
+      <transition
+        appear
+        v-on:enter="onEnterGallery"
+      >
+        <ul class="gallery">
           <li v-for="item in gallery" :key="item.icon">
             <i class="pe" :class="item.icon" aria-hidden="true"></i>
             <h3>{{item.title}}</h3>
             <p>{{item.text}}</p>
           </li>
         </ul>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
+import { TimelineMax, Power4 } from 'gsap'
+import 'waypoints/lib/noframework.waypoints.min'
 export default {
   data: function () {
     return {
@@ -59,6 +69,42 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    onEnter (el, done) {
+      const tl = new TimelineMax({ onComplete: done })
+      const wp1 = new Waypoint({
+        element: el,
+        handler: () => {
+          tl.play()
+        },
+        offset: '80%'
+      })
+
+      tl.set(wp1.element.children, { autoAlpha: 0, top: '20px' })
+      tl.pause()
+      tl.to(el.children[0], 1, { autoAlpha: 1, top: 0, ease: Power4.easeOut })
+      tl.to(el.children[1], 1.8, { autoAlpha: 1, top: 0, ease: Power4.easeOut }, '-=0.8')
+    },
+    onEnterGallery (el, done) {
+      const tl = new TimelineMax({ onComplete: done })
+      const wp2 = new Waypoint({
+        element: el,
+        handler: () => {
+          tl.play()
+        },
+        offset: '80%'
+      })
+
+      tl.set(wp2.element.children, { autoAlpha: 0, top: '10px' })
+      tl.pause()
+      tl.to(el.children[0], 0.6, { autoAlpha: 1, top: 0, ease: Power4.easeOut })
+      tl.to(el.children[1], 0.6, { autoAlpha: 1, top: 0, ease: Power4.easeOut }, '-=0.3')
+      tl.to(el.children[2], 0.6, { autoAlpha: 1, top: 0, ease: Power4.easeOut }, '-=0.3')
+      tl.to(el.children[3], 0.6, { autoAlpha: 1, top: 0, ease: Power4.easeOut }, '-=0.3')
+      tl.to(el.children[4], 0.6, { autoAlpha: 1, top: 0, ease: Power4.easeOut }, '-=0.3')
+      tl.to(el.children[5], 0.6, { autoAlpha: 1, top: 0, ease: Power4.easeOut }, '-=0.3')
+    }
   }
 }
 </script>
@@ -77,8 +123,10 @@ export default {
     h1
       @extend %heading-main
       margin: 0 0 26px
+      position: relative
     p
       @extend %text-main
+      position: relative
       margin-bottom: 60px
       +bp(med)
         padding: 0 40px
@@ -88,45 +136,45 @@ export default {
         margin: 0 auto
         padding: 0 30px
   .gallery
-    ul
+    display: flex
+    flex-wrap: wrap
+    margin: 0
+    padding: 0 0 40px
+    +bp(620)
+      padding: 0 0 100px
+    li
+      position: relative
       display: flex
-      flex-wrap: wrap
-      margin: 0
-      padding: 0 0 40px
+      flex-direction: column
+      align-items: center
+      list-style: none
+      margin: 0 auto
+      padding: 20px 10px
+      width: 100%
+      border-bottom: 1px solid rgba(#9fa6b1, 0.2)
+      &:nth-child(6)
+        border-bottom: 1px solid transparent
+      +bp(400)
+        max-width: 380px
       +bp(620)
-        padding: 0 0 100px
-      li
-        display: flex
-        flex-direction: column
-        align-items: center
-        list-style: none
-        margin: 0 auto
-        padding: 20px 10px
-        width: 100%
-        border-bottom: 1px solid rgba(#9fa6b1, 0.2)
-        &:nth-child(6)
-          border-bottom: 1px solid transparent
-        +bp(400)
-          max-width: 380px
-        +bp(620)
-          max-width: 100%
-          width: 50%
-          &:nth-child(2n+1)
-            border-right: 1px solid rgba(#9fa6b1, 0.2)
-          &:nth-child(5)
-            border-bottom: 1px solid transparent
-        +bp(med)
-          width: 33.33%
+        max-width: 100%
+        width: 50%
+        &:nth-child(2n+1)
           border-right: 1px solid rgba(#9fa6b1, 0.2)
-          &:nth-child(1),
-          &:nth-child(2),
-          &:nth-child(3)
-            padding-top: 0
-            border-bottom: 1px solid rgba(#9fa6b1, 0.2)
-          &:nth-child(4)
-            border-bottom: 1px solid transparent
-          &:nth-child(3n+3)
-            border-right: 1px solid transparent
+        &:nth-child(5)
+          border-bottom: 1px solid transparent
+      +bp(med)
+        width: 33.33%
+        border-right: 1px solid rgba(#9fa6b1, 0.2)
+        &:nth-child(1),
+        &:nth-child(2),
+        &:nth-child(3)
+          padding-top: 0
+          border-bottom: 1px solid rgba(#9fa6b1, 0.2)
+        &:nth-child(4)
+          border-bottom: 1px solid transparent
+        &:nth-child(3n+3)
+          border-right: 1px solid transparent
       i
         color: $cyan
         font-size: 60px

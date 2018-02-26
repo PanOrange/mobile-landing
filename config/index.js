6c1a7a0f96d0953ed6901d65bf603c3c580a41b7
panorange <man.freemailer@gmail.com>
2018-02-26 18:21:07 +0200

    delete "/" in path build

--- config/index.js
+++ config/index.js
@@ -49,8 +49,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
-    assetsSubDirectory: 'mobile-landing/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
